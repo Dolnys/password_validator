eb015fec260791e258b94813e57a6ee7506bcfc0
Dolnys <thedolnys@gmail.com>
2023-05-29 14:13:02 +0200

    final commit

--- script.js
+++ script.js
@@ -4,3 +4,36 @@ const letters = /[a-z]/i;
 const numbers = /[0-9]/;
 const special = /[!@#$%^&*()]/;
 const minValue = 10;
+
+const showMsg = () => {
+  if (
+    pass.value.length > minValue &&
+    pass.value.match(letters) &&
+    pass.value.match(numbers) &&
+    pass.value.match(special)
+  ) {
+    p.textContent = 'You have a very strong password!💪';
+    p.style.color = 'lime';
+  } else if (
+    pass.value.length > minValue &&
+    pass.value.match(letters) &&
+    pass.value.match(numbers)
+  ) {
+    p.textContent = 'You have a good password. 🙂';
+    p.style.color = 'gold';
+  } else {
+    p.textContent = 'You have a weak password! 😕';
+    p.style.color = 'tomato';
+  }
+};
+
+const checkPassword = () => {
+  if (pass.value !== '') {
+    showMsg();
+  } else {
+    p.textContent = "You didn't provide a password...";
+    p.style.color = '';
+  }
+};
+
+pass.addEventListener('keyup', checkPassword);
